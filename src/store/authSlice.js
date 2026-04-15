2c10c5cd4540b555bffef53ce179ffa8ccc65c37
nirob <nirobprottoy12@gmail.com>
2026-04-15 20:44:55 +0600

    redux issue fixed

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,30 +1,26 @@
+// authSlice.js
 import { createSlice } from "@reduxjs/toolkit";
 
-
-const initialState ={
-    status: false,
-    userData:null
-}
-
+const initialState = {
+  status: false,
+  userData: null,
+};
 
 const authSlice = createSlice({
-    name:"auth",
-    initialState,
-    reducers:{ 
-        login:(state,action)=>{
-            state.status = true;
-            state.userData = action.payload.userData
-        },
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+      // FIX: action.payload নিজেই ইউজার ডেটা কন্টেইন করছে
+      state.userData = action.payload;
+    },
+    logout: (state) => {
+      state.status = false;
+      state.userData = null;
+    },
+  },
+});
 
-        logout:(state)=>{
-            state.status = false;
-            state.userData = null;
-        }
-     }
-
-
-})
-
-export const {login,logout} = authSlice.actions;
- 
+export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
